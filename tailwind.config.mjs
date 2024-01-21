87c335a98c1814661aba9834bb59fe98f8db58dd
Kev_CB <kevcb369@gmail.com>
2024-01-21 15:00:50 -0500

    Inicio de proyecto

---
+++ tailwind.config.mjs
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+	theme: {
+		extend: {},
+		backgroundImage: {
+			"open-menu": "url('images/open-menu.svg')",
+			"close-menu": "url('images/close-menu.svg')"
+		}
+	},
+	plugins: [],
+}
