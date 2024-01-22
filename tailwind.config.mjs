3917f3e809da33437ebdb1f387d9bf6f55b0712f
Kev_CB <kevcb369@gmail.com>
2024-01-22 08:58:12 -0500

    Actualización Banner

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -2,10 +2,11 @@
 export default {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
-		extend: {},
-		backgroundImage: {
-			"open-menu": "url('../../images/open-menu.svg')",
-			"close-menu": "url('../../images/close-menu.svg')"
+		extend: {
+			backgroundImage: {
+				"open-menu": "url('../../images/open-menu.svg')",
+				"close-menu": "url('../../images/close-menu.svg')",
+			},
 		},
 	},
 	plugins: [],
